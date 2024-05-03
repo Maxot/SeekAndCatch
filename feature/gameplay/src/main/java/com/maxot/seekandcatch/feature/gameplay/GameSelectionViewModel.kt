64ce78cded2bc893ab394f747378ea9f1a18d2a7
maksymkychuk <max.kichuk@gmail.com>
2024-05-03 13:59:05 +0300

    Game Selection screen update

---
+++ feature/gameplay/src/main/java/com/maxot/seekandcatch/feature/gameplay/GameSelectionViewModel.kt
@@ -0,0 +1,32 @@
+package com.maxot.seekandcatch.feature.gameplay
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.maxot.seekandcatch.data.model.GameDifficulty
+import com.maxot.seekandcatch.data.repository.SettingsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GameSelectionViewModel
+@Inject constructor(
+    private val settingsRepository: SettingsRepository,
+) : ViewModel() {
+
+    val selectedGameDifficulty: StateFlow<GameDifficulty?> =
+        settingsRepository.observeDifficulty().stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Lazily,
+            initialValue = null
+        )
+
+    fun setSelectedDifficulty(gameDifficulty: GameDifficulty) {
+        viewModelScope.launch {
+            settingsRepository.setDifficulty(gameDifficulty)
+        }
+    }
+}
--- feature/gameplay/src/main/java/com/maxot/seekandcatch/feature/gameplay/ui/GameSelectionScreen.kt
+++ feature/gameplay/src/main/java/com/maxot/seekandcatch/feature/gameplay/ui/GameSelectionScreen.kt
@@ -1,6 +1,5 @@
 package com.maxot.seekandcatch.feature.gameplay.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,29 +9,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.maxot.seekandcatch.core.designsystem.theme.SeekAndCatchTheme
 import com.maxot.seekandcatch.data.model.Figure
 import com.maxot.seekandcatch.data.model.GameDifficulty
-import com.maxot.seekandcatch.data.model.Goal
-import com.maxot.seekandcatch.feature.gameplay.FlowGameUiState
-import com.maxot.seekandcatch.feature.gameplay.FlowGameViewModel
+import com.maxot.seekandcatch.feature.gameplay.GameSelectionViewModel
 import com.maxot.seekandcatch.feature.gameplay.R
+import com.maxot.seekandcatch.feature.gameplay.ui.layout.StartGameLayout
 
 @Composable
 fun GameSelectionScreenRoute(
-    viewModel: FlowGameViewModel = hiltViewModel(),
+    viewModel: GameSelectionViewModel = hiltViewModel(),
     navigateToFlowGame: () -> Unit
 ) {
     val selectedDifficulty = viewModel.selectedGameDifficulty.collectAsState()
@@ -52,7 +50,8 @@ fun GameSelectionScreen(
     navigateToFlowGame: () -> Unit,
     onDifficultChanged: (GameDifficulty) -> Unit
 ) {
-    val gameSelectionScreenContentDesc = stringResource(id = R.string.feature_gameplay_game_selection_screen_content_desc)
+    val gameSelectionScreenContentDesc =
+        stringResource(id = R.string.feature_gameplay_game_selection_screen_content_desc)
 
     Column(
         modifier = Modifier
@@ -63,7 +62,8 @@ fun GameSelectionScreen(
         verticalArrangement = Arrangement.Center
     ) {
         StartGameLayout(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth(),
             selectedDifficulty = selectedDifficulty,
             onDifficultyChanged = {
                 onDifficultChanged(it)
