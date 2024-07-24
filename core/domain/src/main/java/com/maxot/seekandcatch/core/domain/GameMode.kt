49dad01c33cd2e77d62c0d96cda4a5dfca2d7d64
maksymkychuk <max.kichuk@gmail.com>
2024-07-24 21:56:34 +0300

    Add GameModes

---
+++ core/domain/src/main/java/com/maxot/seekandcatch/core/domain/GameMode.kt
@@ -0,0 +1,8 @@
+package com.maxot.seekandcatch.core.domain
+
+enum class GameMode(name: String) {
+    FLOW(name = "Flow"),
+    DROP(name = "Drop"),
+    TEST1(name = "Test1"),
+    TEST2(name = "Test2")
+}
