e351f380a50eee4fcd8b7efdd325b0ea2c68a241
maksymkychuk <max.kichuk@gmail.com>
2025-02-27 19:00:47 +0200

    up to java 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,11 +38,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
     buildFeatures {
         prefab = true
