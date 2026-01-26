f0a8d44b266bbd9626b25627c506a8b1bbe8cb95
maksymkychuk <max.kichuk@gmail.com>
2026-01-26 20:47:45 +0200

    ref: life wasted

--- core/common/src/main/java/com/maxot/seekandcatch/core/common/VisualFeedbackManager.kt
+++ core/common/src/main/java/com/maxot/seekandcatch/core/common/VisualFeedbackManager.kt
@@ -1,8 +1,12 @@
 package com.maxot.seekandcatch.core.common
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,11 +15,20 @@ class VisualFeedbackManager @Inject constructor() {
     private val _isLifeWasted = MutableStateFlow(false)
     val isLifeWasted: StateFlow<Boolean> = _isLifeWasted.asStateFlow()
 
-    fun triggerLifeWasted() {
-        _isLifeWasted.value = true
+    private var feedbackJob: Job? = null
+
+    fun triggerLifeWasted(scope: CoroutineScope) {
+        _isLifeWasted.value = false
+        feedbackJob?.cancel()
+        feedbackJob = scope.launch {
+            _isLifeWasted.value = true
+            delay(1000)
+            _isLifeWasted.value = false
+        }
     }
 
     fun resetLifeWasted() {
+        feedbackJob?.cancel()
         _isLifeWasted.value = false
     }
 }
