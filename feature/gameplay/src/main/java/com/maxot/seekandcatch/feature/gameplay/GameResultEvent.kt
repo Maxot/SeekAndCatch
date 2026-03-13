8dd61f5b8afa347155df6e62c04ee585df396ed8
maksymkychuk <max.kichuk@gmail.com>
2026-03-13 20:29:16 +0200

    feat: new record

--- feature/gameplay/src/main/java/com/maxot/seekandcatch/feature/gameplay/GameResultEvent.kt
+++ feature/gameplay/src/main/java/com/maxot/seekandcatch/feature/gameplay/GameResultEvent.kt
@@ -3,4 +3,5 @@ package com.maxot.seekandcatch.feature.gameplay
 sealed interface GameResultEvent {
     data object ContinueClicked : GameResultEvent
     data object RestartClicked : GameResultEvent
+    data object NewBestSoundPlayed : GameResultEvent
 }
